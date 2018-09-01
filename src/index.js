a1591e58da835e29282529ad76858f4e62eaa557
Jimmy <cwu023@gmail.com>
2018-08-31 21:04:33 -0700

    HashRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
